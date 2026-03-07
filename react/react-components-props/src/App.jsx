3e12233f5fbb5a34089f88002e65b23d78beeb99
ANTONIO SERGIO FERREIRA BONATO <antonio.bonato@maua.br>
2026-03-07 13:11:46 -0300

    react(props): ate bloco de codigo 2.2.5

---
+++ react/react-components-props/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+const App = () => {
+  return(
+    //container principal
+    <div className="container border-bottom mt-2">
+      {/* linha para o título */}
+      <h1 className='display-5 text-center'>Seus pedidos</h1>
+    </div>
+  )
+}
+
+export default App
