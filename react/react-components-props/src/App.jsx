5ed4f904951704e080976c5a3adf5369aa15fa10
asbonato <asbonato@gmail.com>
2026-03-14 13:01:48 -0300

    react(props): apostila 4 finalizada

--- react/react-components-props/src/App.jsx
+++ react/react-components-props/src/App.jsx
@@ -1,14 +1,60 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 
-const App = () => {
-  return(
-    //container principal
-    <div className="container border-bottom mt-2">
-      {/* linha para o título */}
-      <h1 className='display-5 text-center'>Seus pedidos</h1>
-    </div>
-  )
-}
+  const App = () => {
+    const textoOK = "Já chegou"
+    const textoNOK = "Ainda não chegou"
+    const funcaoOK = () => alert("Agradecemos pela confirmação!")
+    const funcaoNOK = () => alert("Verificaremos o ocorrido!")
+    const componenteFeedback=<Feedback textoOK={textoOK} funcaoOK={funcaoOK}
+      textoNOK={textoNOK} funcaoNOK={funcaoNOK}/>
+    return(
+      //container principal
+      <div className="container border-bottom mt-2">
+        {/* linha para o título */}
+        <h1 className='display-5 text-center'>Seus pedidos</h1>
+            {/* linha para o primeiro pedido */}
+          <div className="row">
+            {/* controle de colunas para responsividade */}
+            <div className="col-sm-8 col-md-6 m-2">
+            {/* cartão */}
+            <Cartao cabecalho="22/02/2026">
+              <Pedido icone="fas fa-hdd fa-2x"
+               titulo="SSD" descricao="SSD Kingston A400-SATA"/>
+               {componenteFeedback}
+            </Cartao>
+            </div>
+          </div>
+          {/* linha para o segundo pedido */}
+          <div className="row">
+            {/* controle de colunas para responsividade */}
+            <div className="col-sm-8 col-md-6 m-2">
+            {/* cartão */}
+            <Cartao cabecalho="23/02/2026">
+              <Pedido icone="fas fa-book fa-2x"
+                titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
+                {componenteFeedback}
+              </Cartao>
+            </div>
+          </div>
+          {/* linha para o terceiro pedido */}
+          <div className="row">
+            {/* controle de colunas para responsividade */}
+            <div className="col-sm-8 col-md-6 m-2">
+            {/* cartão */}
+            <Cartao cabecalho="20/01/2026">
+              <Pedido icone="fas fa-laptop fa-2x"
+                titulo="SSD" descricao="Notebook Dell - 8Gb - i5"/>
+                {componenteFeedback}
+            </Cartao>
+            </div>
+          </div>
+      </div>
 
-export default App
+    )
+  }
+
+  export default App
---
+++ react/react-components-props/src/Pedido.jsx
@@ -0,0 +1,15 @@
+const Pedido = ({icone, titulo, descricao}) => {
+    return(
+        <div className="d-flex">
+        <div className="d-flex align-items-center">
+            <i className={icone}></i>
+        </div>
+        {/* flex-grow 1: tomar o espaço remanescente */}
+        <div className="flex-grow-1 ms-2 border">
+            <h4 className="text-center">{titulo}</h4>
+            <p className="text-center">{descricao}</p>
+        </div>
+        </div>
+    )
+}
+export default Pedido
